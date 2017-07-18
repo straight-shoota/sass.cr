ce81ff0497e1e2d2c04c4689212e5913821b9ad3
Johannes Müller <straightshoota@gmail.com>
2017-07-18 23:53:50 +0200

    Replace custom autodeploy script straight-shoota/autodeploy-docs

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -1,9 +1,12 @@
 #! /bin/sh
-echo -e "Clearing ./doc directory"
-rm -rf ./doc
+GENERATED_DOCS_DIR="./doc"
+
+echo -e "Building docs into ${GENERATED_DOCS_DIR}"
+echo -e "Clearing ${GENERATED_DOCS_DIR} directory"
+rm -rf "${GENERATED_DOCS_DIR}"
 
 echo -e "Running crystal doc..."
 crystal doc
 
 echo -e "Copying README.md"
-cp README.md "doc/README.md"
+cp README.md "${GENERATED_DOCS_DIR}/README.md"
