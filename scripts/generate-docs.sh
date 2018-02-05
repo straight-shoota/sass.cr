4e5a6581a1723af7e5b5c8045f482394bfcb46bb
Johannes Müller <straightshoota@gmail.com>
2018-02-05 15:04:45 +0000

    Fix scripts/generate-docs.sh to use src/sass.cr as entry point

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
-GENERATED_DOCS_DIR="./doc"
+GENERATED_DOCS_DIR="./docs"
 
 echo "Building docs into ${GENERATED_DOCS_DIR}"
 echo "Clearing ${GENERATED_DOCS_DIR} directory"
 rm -rf "${GENERATED_DOCS_DIR}"
 
 echo "Running crystal doc..."
-crystal doc
+crystal docs src/sass.cr
 
 echo "Copying README.md"
 cp README.md "${GENERATED_DOCS_DIR}/README.md"
