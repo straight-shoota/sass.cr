02f96816d25b3fb09ef255b2fcef053c93f22f81
Johannes Müller <straightshoota@gmail.com>
2018-02-05 18:17:06 +0000

    Use named values for sass2scss *pretty* argument

--- spec/sass2scss_spec.cr
+++ spec/sass2scss_spec.cr
@@ -2,15 +2,15 @@ require "./spec_helper"
 
 describe "Sass.sass2scss" do
   it "converts simple sass" do
-    Sass.sass2scss("p\n  color: red").should eq "p {\n  color: red; }\n"
+    Sass.sass2scss("p\n  color: red").should eq "p\n{\n  color: red;\n}\n"
   end
 
   it "converts with prettify option" do
-    Sass.sass2scss("p\n  color: red", prettify: 0).should eq "p { color: red; }"
-    Sass.sass2scss("p\n  color: red", prettify: 1).should eq "p {\n  color: red; }\n"
-    Sass.sass2scss("p\n  color: red", prettify: 2).should eq "p {\n  color: red;\n}\n"
-    Sass.sass2scss("p\n  color: red", prettify: 3).should eq "p\n{\n  color: red;\n}\n"
-    Sass.sass2scss("p\n  color: red").should eq "p {\n  color: red; }\n"
+    Sass.sass2scss("p\n  color: red", prettify: :minimized).should eq "p { color: red; }"
+    Sass.sass2scss("p\n  color: red", prettify: :lisp_style).should eq "p {\n  color: red; }\n"
+    Sass.sass2scss("p\n  color: red", prettify: :otbs_style).should eq "p {\n  color: red;\n}\n"
+    Sass.sass2scss("p\n  color: red", prettify: :allman_style).should eq "p\n{\n  color: red;\n}\n"
+    Sass.sass2scss("p\n  color: red").should eq "p\n{\n  color: red;\n}\n"
   end
 
   it "converts with comments option" do
--- src/sass2scss.cr
+++ src/sass2scss.cr
@@ -2,17 +2,17 @@ module Sass
   # Converts a `String` with Sass code into SCSS.
   #
   # *prettify* takes the following values:
-  # * `0`: Write everything on one line (`minimized`)
-  # * `1`: Add lf after opening bracket (`lisp style`) *(default)*
-  # * `2`: Add lf after opening and before closing bracket (`1TBS style`)
-  # * `3`: Add lf before/after opening and before closing (`allman style`)
+  # * `:minimized`: Write everything on one line (`minimized`)
+  # * `:lisp_style`: Add lf after opening bracket (`lisp style`)
+  # * `:otbs_style`: Add lf after opening and before closing bracket (`1TBS style`)
+  # * `:allman_style`: Add lf before/after opening and before closing (`allman style`) *(default)*
   #
   # *comments* takes these values:
   # * `:convert`: convert all comments to `/*`and `*/` delimiters *(default)*
   # * `:keep`: keep comments
   # * `:strip`: strip all comments
   #
-  def self.sass2scss(content : String, prettify = 1, comments = :convert) : String
+  def self.sass2scss(content : String, prettify = :allman_style, comments = :convert) : String
     case comments
     when :convert then options = LibSass::Sass2ScssOption::CONVERT_COMMENT
     when :keep    then options = LibSass::Sass2ScssOption::KEEP_COMMENT
@@ -22,10 +22,10 @@ module Sass
     end
 
     case prettify
-    when 0
-    when 1 then options |= LibSass::Sass2ScssOption::PRETTIFY_1
-    when 2 then options |= LibSass::Sass2ScssOption::PRETTIFY_2
-    when 3 then options |= LibSass::Sass2ScssOption::PRETTIFY_3
+    when 0, :minimized
+    when 1, :lisp_style   then options |= LibSass::Sass2ScssOption::PRETTIFY_1
+    when 2, :otbs_style   then options |= LibSass::Sass2ScssOption::PRETTIFY_2
+    when 3, :allman_style then options |= LibSass::Sass2ScssOption::PRETTIFY_3
     else
       raise ArgumentError.new "invalid prettify option: #{prettify.inspect}"
     end
